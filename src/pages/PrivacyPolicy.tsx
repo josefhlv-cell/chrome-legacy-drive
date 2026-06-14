b2f5bcdd02c89c83619c0ba9d11543ad190c9b22
Robin <josefhlv@gmail.com>
2026-06-14 13:45:12 +0200

    Create PrivacyPolicy.tsx

---
+++ src/pages/PrivacyPolicy.tsx
@@ -0,0 +1,28 @@
+export default function PrivacyPolicy() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Zásady ochrany osobních údajů</h1>
+
+      <p>
+        Aplikace CHDP Garage může zpracovávat údaje potřebné pro správné
+        fungování aplikace.
+      </p>
+
+      <p>
+        Aplikace nesdílí osobní údaje s třetími stranami.
+      </p>
+
+      <p>
+        Kontakt: Chrysler Dodge Pardubice
+      </p>
+
+      <p>
+        Web: https://chryslerpardubice.site
+      </p>
+
+      <p>
+        Poslední aktualizace: 14. 6. 2026
+      </p>
+    </div>
+  );
+}
